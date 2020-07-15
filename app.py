cfe9bbd030b4365c890f77557bc1f1f4e91021ab
rahul ramesh <rahulramesh2109@gmail.com>
2020-07-15 17:51:54 +0530

    base64 version of py_flask_ocr

--- app.py
+++ app.py
@@ -3,40 +3,31 @@ import os
 
 from ocr_core import ocr_core
 
-Upload_file = '/static/uploads'
 
-allowed_extns = set(['png','jpg','jpeg'])
 
 app = Flask(__name__)
 
-def allowed_file(fname):
-	return '.' in fname and \
-			fname.rsplit('.',1)[1].lower() in allowed_extns
-
-
-@app.route('/')
-def home_page():
-	return render_template('index.html')
-	# return "Hello"
-
-
-@app.route('/upload',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def upload_page():
 	if request.method == "POST":
-		if 'file' not in request.files:
-			return render_template('upload.html',msg='No file selected')
-		file = request.files['file']
-		if file.filename == '':
+
+		data = request.form['url']
+		if data is None:
+			return render_template('upload.html',msg='No files selected')
+		
+		file = data
+
+		if file== '':	
 			return render_template('upload.html',msg='No file selected')
 
-		if file and allowed_file(file.filename):
+		if file :
 			extracted_text = ocr_core(file)
 			return render_template('upload.html',
 									msg='Succesfully processed',
 									extracted_text = extracted_text,
-									img_src = Upload_file + file.filename)
+									img_src = file)
 	elif request.method == "GET":
 		return render_template('upload.html')
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
--- ocr_core.py
+++ ocr_core.py
@@ -3,33 +3,37 @@ try:
 except ImportError:
 	import Image
 import pytesseract
+# from pytesseract import Output #for image_to_data_implementation
+import base64
+import io
+import numpy as np
+# import sys
+# sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+import cv2 #for preprocessing
+# sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 
 def ocr_core(fname):
-	text = pytesseract.image_to_string(Image.open(fname))
+	imgstring = fname.split('base64,')[-1].strip()
+	pic = io.StringIO()
+	image_string = io.BytesIO(base64.b64decode(imgstring))
+	convert_and_save(imgstring)
+	image = Image.open(image_string)
+
+	img = cv2.imread("imageToSave.png")
+	# image preprocessing
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #grayscalling the image
+	kernel = np.ones((5,5),np.uint8)
+	cv2.dilate(img, kernel, iterations = 2) #dilation
+	cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1] #thresholding
+	# cv2.Canny(img, 100, 200) # canny edge detection
+	custom_config = r'--oem 2 --psm 12' # psm: 12 = sparse text with osd , oem: 2 = Legacy+LSTM engines
+	text = pytesseract.image_to_string(img, config=custom_config, lang="eng+hin")
+	# attempted implementation wih image_to_data
+	# d = pytesseract.image_to_data(img, output_type=Output.DICT)
+	# return d.keys()
 	return text
-print(ocr_core('/home/rahul/Pictures/py_ocr_test_13072020.png'))
 
+def convert_and_save(b64_string):
+    with open("imageToSave.png", "wb") as fh:
+        fh.write(base64.b64decode(b64_string))
 
-
-# try:
-# 	from PIL import Image
-# except ImportError:
-# 	import Image
-# import pytesseract
-# import base64
-# import io
-# import cv2 #for preprocessing
-# def ocr_core(fname):
-# 	imgstring = fname.split('base64,')[-1].strip()
-# 	pic = io.StringIO()
-# 	image_string = io.BytesIO(base64.b64decode(imgstring))
-# 	image = Image.open(image_string)
-# 	# Overlay on white background
-# 	bg = Image.new("RGB", image.size, (255,255,255))
-# 	bg.paste(image,image)
-# 	img = cv2.imread(bg)
-# 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# 	img = cv2.medianBlur(img,5)
-# 	text = pytesseract.image_to_string(img)
-# 	return text
-# # print(ocr_core('/home/rahul/Pictures/py_ocr_test_13072020.png'))
